00df507c325e53e82acabfeae776f9e42cec4a59
biubiu33 <630440438@qq.com>
2026-04-30 15:05:37 +0800

    添加随机时间

--- checkin.py
+++ checkin.py
@@ -21,9 +21,10 @@ def start():
         time = time.split('.')[0]
         # print(time)
         if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?title=' + mess + '，you have ' + time + ' days left')
+            content = mess + '，you have ' + time + ' days left'
+            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?title=glados自动签到&desp='+content)
     else:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?title=cookie过期')
+        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?title=glados自动签到&desp=cookie过期')
 
 
 def main_handler(event, context):
@@ -34,7 +35,7 @@ if __name__ == '__main__':
     import time
     import random
     # 在正式请求前随机等待 1 到 600 秒 (10分钟内)
-    sleep_time = random.randint(1, 600)
+    sleep_time = random.randint(1, 10)
     print(f"随机等待 {sleep_time} 秒后再执行签到...")
     time.sleep(sleep_time)
     start()
